cef16e2ed02f91ab24e384c2ed03de5d78a4e870
galen <galen_chou@163.com>
2023-02-22 16:17:11 +0800

    feat:button初始化

---
+++ src/button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="my-button">{{ title }}</button>
+</template>
+<script>
+export default {
+  name: "MyButton",
+  data() {
+    return {
+      title: "我的第一个组件库button",
+    };
+  },
+};
+</script>
+<style scoped>
+.my-button {
+  color: red;
+}
+</style>
